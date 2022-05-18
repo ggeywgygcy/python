81c970567a027a6ca8d0772476a63243970a9547
ggeywgygcy <wasilij18bus@gmail.com>
2022-05-18 10:27:43 +0300

    ghbdtn

--- 01.py
+++ 01.py
@@ -1,3 +1,12 @@
-print('вставай донбасс')
-l = input('введите текст')
-print('привет'+l)
+import random
+i = random.randit(1,100)
+s = random.randit(1,100)
+i = 7
+s = 7
+print(i)
+print(s)
+if i == s
+    print(str(i)+' = '+str(s))
+else:
+    print(str(i)+' = '+str(s))
+
