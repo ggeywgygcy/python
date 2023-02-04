a8115f173f5235dff50057ea7be9d8457cf3be1d
ggeywgygcy <wasilij18bus@gmail.com>
2023-02-04 13:55:47 +0300

    gtftdftfdc

--- forma3.py
+++ forma3.py
@@ -1,6 +1,18 @@
 from tkinter import *
 
 def creat_triangle(canvas,x1,y1,x2,y2,x3,y3):
+    
+    
+    х1 = input('введите первую координату для х1 ')
+    y1 = input('введите вторую координату для х1 ')
     canvas.create_line(x1,y1,x2,y2)
+    
+    х2 = input('введите первую координату для х2 ')
+    y2 = input('введите вторую координату для х2 ')
     canvas.create_line(x1,y1,x3,y3)
+    
+    х3 = input('введите первую координату для х3 ')
+    y3 = input('введите вторую координату для х3 ')
+
     canvas.create_line(x3,y3,x2,y2)
+   
--- forma5.py
+++ forma5.py
@@ -4,6 +4,6 @@ from forma3 import creat_triangle
 root = Tk()
 root.title('Треугльник')
 canvas = Canvas(root,width=800,height=600,background='white')
-creat_triangle(input('Введите координату x для первой вершины '))
+creat_triangle(canvas,100,50,100,200,400,200)
 canvas.pack(expand=True,fill=BOTH)
 root.mainloop() 
