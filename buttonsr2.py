94ea4a0f312ba0fc80a24723f09dc7b6ea3b868a
ggeywgygcy <wasilij18bus@gmail.com>
2023-02-18 12:07:30 +0300

    gnirebifgbre

--- buttonsr2.py
+++ buttonsr2.py
@@ -4,39 +4,43 @@ def button1():
     b1['text'] = 'Изменено...'
     b1['bg'] = '#ff0000'
     l1['text'] = 'красный'
+    b1.config(command=button1)
 
 def button2():
     b2['text'] = 'Изменено...'
     b2['bg'] = '#ff7d00'
     l2['text'] = 'оранжевый'
-
+    b2.config(command=button2)
 
 def button3():    
     b3['text'] = 'Изменено...'
     b3['bg'] = '#ffff00'
     l3['text'] = 'желтый'
-
+    b3.config(command=button3)
 
 def button4():
     b4['text'] = 'Изменено...'
     b4['bg'] = '#00ff00'
     l4['text'] = 'зеленый'
-
+    b4.config(command=button4)
 
 def button5():
     b5['text'] = 'Изменено...'
     b5['bg'] = '#007dff'
     l5['text'] = 'красный'
+    b5.config(command=button5)
 
 def button6():
     b6['text'] = 'Изменено...'
     b6['bg'] = '#0000ff'
     l6['text'] = 'красный'
+    b6.config(command=button6)
 
 def button7():
     b7['text'] = 'Изменено...'
     b7['bg'] = '#7d00ff'
     l7['text'] = 'красный'
+    b7.config(command=button7)
 
 root = Tk()
 
@@ -56,13 +60,13 @@ l5 = Label(text="Изменить",width=15,height=1)
 l6 = Label(text="Изменить",width=15,height=1)
 l7 = Label(text="Изменить",width=15,height=1)
 
-b1.config(command=button1)
-b2.config(command=button2)
-b3.config(command=button3)
-b4.config(command=button4)
-b5.config(command=button5)
-b6.config(command=button6)
-b7.config(command=button7)
+
+
+
+
+
+
+
 
 l1.config(command=button1)
 l2.config(command=button2)
