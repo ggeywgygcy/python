4cef70e469f8066166c4c04cb586b18b283f6638
ggeywgygcy <wasilij18bus@gmail.com>
2023-02-18 13:51:07 +0300

    bfigberiyfb

--- butt3onTou.py
+++ butt3onTou.py
@@ -2,14 +2,21 @@ from tkinter import *
 
 root = Tk()
 
+#fr_top = Frame(root)
+#fr_top = Frame(root)
+
+
 l1 = Label(width=7,height=4,bg='yellow',text='1') 
 l2 = Label(width=7,height=4,bg='red',text='0')
 l3 = Label(width=7,height=4,bg='purple',text='6')
 l4 = Label(width=7,height=4,bg='white',text='12')
 
-l1.pack(side=TOP)
-l2.pack(side=BOTTOM)
-l3.pack(side=TOP)
-l4.pack(side=BOTTOM)
+l1.grid(row=0,column=0)
+l2.grid(row=0,column=1)
+l3.grid(row=1,column=0)
+l4.grid(row=1,column=1)
+
+#fr_top.pack()
+#fr_top.pack()
 
 root.mainloop()
---
+++ buttonsr.py.py
@@ -0,0 +1,65 @@
+from tkinter import *
+
+def button1():
+    a1['text'] = 'Красный'
+    b1.delete(0,END)
+    b1.insert(0,'#ff0000')
+
+def button2():
+    a1['text'] = 'Оранжевый'
+    b1.delete(0,END)
+    b1.insert(0,'#ff7d00')
+
+def button3():
+    a1['text'] = 'Желтый'
+    b1.delete(0,END)
+    b1.insert(0,'#ffff00')
+
+def button4():
+    a1['text'] = 'Зеленый'
+    b1.delete(0,END)
+    b1.insert(0,'#00ff00')
+
+def button5():
+    a1['text'] = 'Голубой'
+    b1.delete(0,END)
+    b1.insert(0,'#007dff')
+
+def button6():
+    a1['text'] = 'Синий'
+    b1.delete(0,END)
+    b1.insert(0,'#0000ff')
+
+def button7():
+    a1['text'] = 'Фиолетовый'
+    b1.delete(0,END)
+    b1.insert(0,'#FF7518')
+
+root = Tk()
+
+fr = Frame(root)
+
+a1 = Label(width=50,anchor=CENTER)
+b1 = Entry(width=50,justify=CENTER)
+l1 = Button(fr,width=1,command=button1,bg='#ff0000')
+l2 = Button(fr,width=1,command=button2,bg='#ff7d00')
+l3 = Button(fr,width=1,command=button3,bg='#ffff00')
+l4 = Button(fr,width=1,command=button4,bg='#00ff00')
+l5 = Button(fr,width=1,command=button5,bg='#007dff')
+l6 = Button(fr,width=1,command=button6,bg='#0000ff')
+l7 = Button(fr,width=1,command=button7,bg='#FF7518')
+
+a1.pack()
+b1.pack()
+l1.pack(side=LEFT)
+l2.pack(side=LEFT)
+l3.pack(side=LEFT)
+l4.pack(side=LEFT)
+l5.pack(side=LEFT)
+l6.pack(side=LEFT)
+l7.pack(side=LEFT)
+
+fr.pack()
+
+
+root.mainloop()
